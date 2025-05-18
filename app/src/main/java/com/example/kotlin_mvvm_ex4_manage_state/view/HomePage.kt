fd5cc356ea7a9a559bff102c8c7f8a6d79b493a2
kells\KellsPing <kellsping@gmail.com>
2025-05-18 17:20:45 +0800

    Add remember function.

--- app/src/main/java/com/example/kotlin_mvvm_ex4_manage_state/view/HomePage.kt
+++ app/src/main/java/com/example/kotlin_mvvm_ex4_manage_state/view/HomePage.kt
@@ -1,5 +1,6 @@
 package com.example.kotlin_mvvm_ex4_manage_state.view
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -8,6 +9,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
@@ -16,28 +21,41 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomePage() {
+    Log.i("HomePage", "is called.")
+
+    var name by remember {
+        mutableStateOf("")
+    }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        MyText()
-        MyTextField()
+        Text(text = "Home Page", style = TextStyle(fontSize = 30.sp))
+        Spacer(Modifier.height(30.dp))
+
+        // remember: persist state on recomposition
+        MyText(name)
+        MyTextField(name, onNameChange = {
+            name = it
+        })
+        Spacer(Modifier.height(30.dp))
     }
 }
 
 @Composable
-fun MyText() {
-    Text(text = "Home Page", style = TextStyle(fontSize = 30.sp))
-    Spacer(Modifier.height(30.dp))
-    Text(text = "Name:")
+fun MyText(name: String) {
+    Text(text = "Name: $name")
 }
 
 @Composable
-fun MyTextField() {
+fun MyTextField(name: String, onNameChange: (String) -> Unit) {
     OutlinedTextField(
-        value = "",
-        onValueChange = {},
+        value = name,
+        onValueChange = {
+            onNameChange(it)
+        },
         label = { Text(text = "Enter name") }
     )
 }
